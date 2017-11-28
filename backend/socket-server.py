f2106fd49a43624ae22a970c47300aeff289173c
Paavo Pärssinen <paavo.parssinen@aalto.fi>
2017-11-28 14:40:01 +0200

    Added function checking when server receives request

--- backend/socket-server.py
+++ backend/socket-server.py
@@ -1,16 +1,26 @@
 import subprocess
 from bottle import run, post, request, response, get, route
 import logging
+import glob, os
 
 logging.basicConfig(filename='logs/server.log',format='%(asctime)s %(message)s', level=logging.DEBUG)
 
+# Checking the available functions
+functions = glob.glob("functions/*.py")
+print(functions)
+
 @route('/<path>',method = 'POST')
 def process(path):
-    postdata = request.body.read()
-    logging.debug("Server received request: "+path)
-    try:
-        return subprocess.check_output(['python3','functions/'+path+'.py', postdata],shell=False)
-    except subprocess.CalledProcessError as e:
-        logging.debug("Server could not run function: "+path)
+    function = 'functions/'+path+'.py'
+    if function in functions:
+        postdata = request.body.read()
+        logging.debug("Server received request: "+path)
+        try:
+            print(os.listdir())
+            return subprocess.check_output(['python3',function, postdata],shell=False)
+        except subprocess.CalledProcessError as e:
+            logging.debug("Server could not run function: "+path)
+    else:
+        return path+" not available"
 
 run(host='localhost', port=8080, debug=True, server='paste')
